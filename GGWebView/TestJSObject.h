a0d1db067f67adf454af9b967d7a35460aae16a8
__无邪_ <Otherplayer@GuoledeMacBook-Pro.local>
2015-09-23 13:15:48 +0800

    first

---
+++ GGWebView/TestJSObject.h
@@ -0,0 +1,13 @@
+//
+//  TestJSObject.h
+//  GGWebView
+//
+//  Created by __无邪_ on 15/9/23.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestJSObject : NSObject
+
+@end
---
+++ GGWebView/TestJSObject.m
@@ -0,0 +1,13 @@
+//
+//  TestJSObject.m
+//  GGWebView
+//
+//  Created by __无邪_ on 15/9/23.
+//  Copyright © 2015年 __无邪_. All rights reserved.
+//
+
+#import "TestJSObject.h"
+
+@implementation TestJSObject
+
+@end
